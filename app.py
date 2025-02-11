f4b5d82224a31ed799687f07e95a92af9d060516
Harika <harika7597@gmail.com>
2025-02-11 12:27:36 -0500

    App is committing

---
+++ app.py
@@ -0,0 +1,11 @@
+print("Hello World")
+print(input("Enter your name: "))
+num = 34
+if(num > 12):
+    print("number is good")
+else:
+    print("number is bad")
+
+def hello():
+    return "Hello World"
+print(hello())
