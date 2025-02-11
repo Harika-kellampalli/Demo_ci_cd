84925e7fda2250ed13809f4f53d7d30b626b016d
Harika <harika7597@gmail.com>
2025-02-11 13:38:28 -0500

    Fix EOFError by handling input in __main__

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-print("Hello World")
-print(input("Enter your name: "))
-num = 34
-if(num > 12):
-    print("number is good")
-else:
-    print("number is bad")
-
 def hello():
     return "Hello World"
-print(hello())
+
+# Ensure input() doesn't break automated tests
+if __name__ == "__main__":
+    print("Hello World")
+    name = input("Enter your name: ")
+    print(name)
+
+    num = 34
+    if num > 12:
+        print("number is good")
+    else:
+        print("number is bad")
+
+    print(hello())
